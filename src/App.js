5b852efeecaea4d3398312b06a12a393826ffa42
Po Lien Lin <polien.joe.lin@gmail.com>
2023-07-13 20:50:43 +0800

    Link to Clarifai face-detection API

--- src/App.js
+++ src/App.js
@@ -2,38 +2,77 @@
 import './App.css';
 import Navigation from "./Components/Navigation/Navigation";
 import Logo from "./Components/Logo/Logo";
-import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
 import Rank from "./Components/Rank/Rank";
+import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
+import FaceRecognition from "./Components/FaceRecognition/FaceRecognition";
 import React, { Component } from 'react';
 import 'tachyons';
 import ParticlesBg from 'particles-bg';
+// import Clarifai from 'clarifai';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Navigation />
-//       {/* <Logo />
-//       <rank/>
-//       <ImageLinkForm  />
-//       <FaceRecognition  /> */}
-//     </div>
-//   );
-// }
+const MODEL_ID = 'face-detection';
+const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';  
+
+
+const returnRequestOptions = (imageUrl) => {
+  
+  const PAT = 'e429cbb5db254b2482b6ebbe4d76f656';
+  const USER_ID = 'imladris20';       
+  const APP_ID = 'face_recognition_brain';
+  // const MODEL_ID = 'face-detection';
+  // const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+                  }
+              }
+          }
+      ]
+  });
+
+  const requestOptions = {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+      },
+      body: raw
+  };
+
+  return requestOptions;
+}
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      input: "",
+      input: '',
+      imageUrl: ''
     }
   }
 
   onInputChange = (event) => {
     console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
 
   onButtonClick = () => {
     console.log("You just clicked");
+    this.setState({imageUrl: this.state.input});
+
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", returnRequestOptions(this.state.input))  //  如果括號裡面寫(this.state.imageUrl)會發生400錯誤，可試試看，這是進階議題
+      .then(response => response.json())
+      .then(result => console.log(result.outputs[0].data.regions[0].region_info.bounding_box))
+      .catch(error => console.log('error', error));
   }
 
 
@@ -48,7 +87,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonClick={this.onButtonClick}
         />
-        {/* <FaceRecognition  /> */}
+        <FaceRecognition  imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
