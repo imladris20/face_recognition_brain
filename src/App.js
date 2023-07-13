02a6a3afa009582bf87237ed18a761749ff941a9
Po Lien Lin <polien.joe.lin@gmail.com>
2023-07-13 14:47:32 +0800

    Add onchange function prototype

--- src/App.js
+++ src/App.js
@@ -21,6 +21,18 @@ import ParticlesBg from 'particles-bg';
 // }
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: "",
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+
   render(){
     return (
       <div className="App">
@@ -28,7 +40,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} />
         {/* <FaceRecognition  /> */}
       </div>
     );
--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -2,7 +2,7 @@ import React from "react";
 import 'tachyons';
 import './ImageLinkForm.css'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange}) => {
     return (
         <div>
             <p className="f3 white">
@@ -10,7 +10,7 @@ const ImageLinkForm = () => {
             </p>
             <div className="common">
                 <div className="common pa4 br3 shadow-5 form">
-                    <input className="f4 pa2 w-70 center" type='text' />
+                    <input className="f4 pa2 w-70 center" type='text' onChange={onInputChange}/>
                     <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
                 </div>
             </div>
